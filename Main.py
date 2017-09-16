be9e6f369f6d0f674ee0f887e7bbe2e3dd380801
tylerBernero <tyler.bernero@gmail.com>
2017-09-16 17:01:48 -0500

    Modified .py files so Klaus can code

--- Main.py
+++ Main.py
@@ -2,10 +2,38 @@
 
 import Menu
 
+
 # display program name and menu options
 menu1 = Menu.Menu()
 menu1.displayProgramName()
 menu1.displayMenu()
+
+print("\nPlease enter the number of your choice?")
+choice = int(input())
+while choice > 0 or choice < 6:
+
+    print(choice)
+    if choice == 1:
+        menu1.add()
+        choice = 0
+    elif choice == 2:
+        menu1.remove()
+        choice = 0
+    elif choice == 3:
+        menu1.edit()
+        choice = 0
+    elif choice == 4:
+        menu1.swap()
+        choice = 0
+    elif choice == 5:
+        menu1.display()
+        choice = 0
+#print("That is not a valid integer. Please enter number between 1 and 5")
+#choice = int(input())
+
+
+
+
 
 
 
--- Menu.py
+++ Menu.py
@@ -10,6 +10,7 @@ class Menu:
         self.menuOptions.append("3) Edit item in list.")
         self.menuOptions.append("4) Swap 2 tasks.")
         self.menuOptions.append("5) Display the list.")
+        self.toDoTasks = []
         return
 
     def displayProgramName(self):
@@ -20,3 +21,23 @@ class Menu:
         for option in range(len(self.menuOptions)):
             print(self.menuOptions[option])
         return
+
+    def add(self):
+        print("Please enter the name of the task you would like to add")
+        task = input()
+        self.toDoTasks.append(task)
+        print(self.toDoTasks)
+        return
+
+    def remove(self):
+        return
+
+    def edit(self):
+        return
+
+    def swap(self):
+        return
+
+    def display(self):
+        return
+
